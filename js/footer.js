8ad9806214085d4208e172b8d72259147870f5bd
plexion <46572320+plexiondev@users.noreply.github.com>
2022-05-19 16:39:39 +0100

    merge web stuff from `skyplex` to `skyplex-web`

---
+++ js/footer.js
@@ -0,0 +1,16 @@
+
+// load footer
+
+
+document.getElementById('footer').innerHTML =
+(`
+<div class="footer-inner">
+<span class="main">
+    <p>plexion 2022.</p>
+</span>
+<span class="links">
+    <a href="/">Home</a>
+    <a href="https://github.com/skyplex">View Source</a>
+</span>
+</div>
+`);
