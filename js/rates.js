1e728504c27272606a2ad2e5621cef85cae4b39a
plexion <46572320+plexiondev@users.noreply.github.com>
2022-05-28 14:27:43 +0100

    works

--- js/rates.js
+++ js/rates.js
@@ -13,8 +13,19 @@ function get_rates() {
     $.get('/generate/rates.json',function(data) {
         for (let type in data) {
             create_type_category(type);
-            for (let i in data[`${type}`]) {
-                console.log(type,i,data[`${type}`][i])
+            for (let n in data[`${type}`]) {
+                for (let i in data[`${type}`][n]) {
+                    let em_record = document.createElement('tr');
+                    em_record.innerHTML = (`
+                    <th class="level"><code>LVL ${parseInt(n)+1}</code></th>
+                    <th class="icon"><div class="headline-icon min" style="padding: 0; height: auto; position: relative; top: 5px;"><img src="https://plexion.dev/img/item/${data[`${type}`][n][i].name}.png"</div></th>
+                    <th class="name">${data[`${type}`][n][i].name}</th>
+                    <th class="values"><code class="on no-icon">+${data[`${type}`][n][i].amount}</code></th>
+                    `);
+    
+                    // append
+                    document.getElementById(`category.${type}.table`).appendChild(em_record);
+                }
             }
         }
     });
@@ -22,11 +33,25 @@ function get_rates() {
 
 // create category panel
 function create_type_category(type) {
-    let em_panel = document.createElement('div');
+    let em_panel = document.createElement('section');
     em_panel.classList.add('left','header','sep','no-align');
     em_panel.style = 'top: 0;';
-
     em_panel.id = `category.${type}`;
+    em_panel.innerHTML = (`
+    <h4>${types[type]}</h4>
+    <div class="customising-cont">
+        <table class="customising">
+            <tbody id="category.${type}.table">
+                <tr>
+                    <th class="level">Level</th>
+                    <th class="icon">Icon</th>
+                    <th class="name">Name</th>
+                    <th class="values">Amount</th>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    `);
 
     // append
     document.getElementById('rates_append').appendChild(em_panel);
