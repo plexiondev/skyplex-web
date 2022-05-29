dd5e481b60807276b621ea860a8d5a69ddc458ce
plexion <46572320+plexiondev@users.noreply.github.com>
2022-05-30 00:46:26 +0100

    make it work

--- js/trades.js
+++ js/trades.js
@@ -25,5 +25,21 @@ function select() {
 function generate() {
     document.getElementById('cont').innerHTML = '';
 
+    let trade = document.getElementById('trade').value;
 
+    let output = `give @p villager_spawn_egg{display:{Name:'{"text":"${trade} Villager Spawn Egg","italic":false}'},EntityTag:{Offers:{Recipes:[ 1`;
+
+    for (let n in data) {
+        if (n != 'trades') {
+            // not trades
+
+            for (let i in data[n]) {
+                output = `${output}{buy:{id:"minecraft:${data[n][i].name}",Count:${data[n][i].quantity}b},sell:{id:"minecraft:gold_nugget",Count:${data[n][i].sell}b}}`
+            }
+        }
+    }
+
+    // end
+    output = `${output}]}}}`;
+    document.getElementById('cont').innerHTML = `${output}`;
 }
