a64f29f56bf09836901b7bdd60f1f3918a79da1c
plexion <46572320+plexiondev@users.noreply.github.com>
2022-05-30 14:51:01 +0100

    give villager effects

--- js/trades.js
+++ js/trades.js
@@ -46,7 +46,7 @@ function generate() {
     document.getElementById('attr.name').textContent = `${name}`;
     document.getElementById('attr.profession').textContent = `${profession}`;
 
-    let output = `give @p villager_spawn_egg{display:{Name:'{"text":"${name} Villager Spawn Egg","italic":false}'},EntityTag:{CustomName:'{"text":"${name}","italic":false}',VillagerData:{profession:"minecraft:${profession}"},NoAI:1b,Offers:{Recipes:[`;
+    let output = `give @p villager_spawn_egg{display:{Name:'{"text":"${name} Villager Spawn Egg","italic":false}'},EntityTag:{ActiveEffects:[{Id:10b,Amplifier:255b,Duration:9999},{Id:11b,Amplifier:2555b,Duration:9999},{Id:12b,Amplifier:2555b,Duration:9999}],CustomName:'{"text":"${name}","italic":false}',VillagerData:{profession:"minecraft:${profession}"},NoAI:1b,Offers:{Recipes:[`;
 
     let count = 0;
     for (let n in data) {
