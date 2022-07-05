91916e17f17390a9db84e28870391c7d110c9af2
plexion <46572320+plexiondev@users.noreply.github.com>
2022-07-05 20:55:22 +0100

    make level-calc generator work

--- js/levels.js
+++ js/levels.js
@@ -1,7 +1,28 @@
 // level-calc generator
 
 
-// store versions/iterations
-// to be used in selector?
-const versions = [2022.0705];
+// the amount of lines to create
+const max = 300;
 
+// nth term
+const nth = 100;
+// base number
+const base = 400;
+
+generate_levels('levels');
+
+/**
+ * level-calc generator
+ * @param {string} append element ID to append data to
+ */
+function generate_levels(append) {
+    let text_level = '';
+    for (let i = 1; i <= max; i++) {
+        let value = (nth * i) + base;
+
+        text_level = `${text_level}scoreboard players set generic.${i} lvl.goal ${value}<br>`;
+    }
+
+    document.getElementById('head').innerHTML = `${nth}n + ${base}`;
+    document.getElementById(append).innerHTML = text_level;
+}
